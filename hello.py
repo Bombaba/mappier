b7ee9c11564373ef479a85dd6dc7ad2c7994db18
Hidesato Ikeya <iky.hdst@gmail.com>
2017-12-25 00:23:42 +0900

    modified css

--- hello.py
+++ hello.py
@@ -1,23 +1,28 @@
-from flask import Flask
+import datetime
 import os
+import flask
 #import cf_deployment_tracker
 
 #cf_deployment_tracker.track()
 
 port = int(os.getenv('PORT', 8000))
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 @app.route("/<title>")
 def home(title=""):
-    if not title:
-        title = "Hello!"
-
-    with open("./public/index.html") as f:
-        src = f.read()
-
-    return src.format(title=title)
+    timenow = datetime.datetime.now()
+    last_visit = flask.request.cookies.get('last-visit') or timenow
+    response = flask.make_response(
+        flask.render_template("index.html", 
+                              title=title,
+                              last_visit=last_visit)
+    )
+    expires = timenow + datetime.timedelta(days=7)
+    response.set_cookie("last-visited", timenow.isoformat(),
+                        expires=expires)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
