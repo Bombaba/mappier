fd97a9898ffb454a31365b954628c34ac498673c
Hidesato Ikeya <iky.hdst@gmail.com>
2017-12-26 14:49:59 +0900

    added google map

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import flask
+import myconfig
 #import cf_deployment_tracker
 
 #cf_deployment_tracker.track()
@@ -10,14 +11,12 @@ port = int(os.getenv('PORT', 8000))
 app = flask.Flask(__name__)
 
 @app.route("/")
-@app.route("/<title>")
-def home(title=""):
+def home():
     timenow = datetime.datetime.now()
     last_visit = flask.request.cookies.get('last-visit') or timenow
     response = flask.make_response(
         flask.render_template("index.html", 
-                              title=title,
-                              last_visit=last_visit)
+                              map_key=myconfig.gmap_key)
     )
     expires = timenow + datetime.timedelta(days=7)
     response.set_cookie("last-visited", timenow.isoformat(),
