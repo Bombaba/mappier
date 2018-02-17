dd595e08884d018af78a0bb56a8a39e8f07df470
Hidesato Ikeya <iky.hdst@gmail.com>
2018-02-17 13:38:38 +0900

    use openstreetmap

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -3,11 +3,12 @@
 const map = L.map('mapid').fitWorld();
 
 L.tileLayer(
-  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 20,
-  id: 'mapbox.streets',
-  accessToken: "pk.eyJ1IjoiaWt5aGRzdCIsImEiOiJjamM3bXoyN3IxZmR2Mnl0OHZzZWJwNW9tIn0._7_CrTVqam54BfRuz6-17Q"})
+//  id: 'mapbox.streets',
+//  accessToken: "pk.eyJ1IjoiaWt5aGRzdCIsImEiOiJjamM3bXoyN3IxZmR2Mnl0OHZzZWJwNW9tIn0._7_CrTVqam54BfRuz6-17Q"
+})
 .addTo(map);
 
 map.locate({setView: true, maxZoom: 18});
@@ -18,12 +19,16 @@ map.on('locationfound', (e)=>{
   .bindPopup(`You are within <strong>${radius}</strong> meters from this point`)
   .openPopup();
 
-  L.circle(e.latlng,
-    { radius: radius,
+  L.circle(e.latlng, {
+      radius: radius,
       color: 'red',
       fillColor: '#f03',
-      fillOpacity: 0.5})
+      fillOpacity: 0.5
+  })
   .addTo(map);
 });
 
-map.on('locationerror', (e)=>alert(e.massage));
+map.on('locationerror', (e)=>{
+  alert(e.message);
+  map.setView([34.725401, 137.718005], 18);
+});
