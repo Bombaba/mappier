38ae54c0077d5ecedbd81baa27db177c5dace083
Hidesato Ikeya <iky.hdst@gmail.com>
2018-02-18 21:16:32 +0900

    implemented firebase

--- hello.py
+++ hello.py
@@ -12,21 +12,26 @@ app.config.from_pyfile('config.py')
 
 @app.route("/")
 def home():
+    response = flask.make_response(
+        flask.render_template("auth.html"))
+    return response
+
+@app.route("/map")
+def map():
     timenow = datetime.datetime.now()
     last_visit = flask.request.cookies.get('last-visit') or timenow
     response = flask.make_response(
-        flask.render_template("index.html"))
+        flask.render_template("map.html"))
     expires = timenow + datetime.timedelta(days=7)
     response.set_cookie("last-visited",
                         timenow.isoformat(),
                         expires=expires)
     return response
-
 @app.after_request
 def add_header(response):
     response.cache_control.no_store = True
     return response
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=port, debug=False)
-    app.run(port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+    #app.run(port=port, debug=True)
--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,5 +1,24 @@
 "use strict";
 
+const userid = "ikyhdst1234"
+
+function getCenter(map) {
+  const {lat, lng} = map.getCenter();
+  return [lat, lng];
+}
+
+function getRadius(map) {
+  const bounds = map.getBounds();
+  const sw = bounds.getSouthWest();
+  const ne = bounds.getNorthEast();
+  // The unit of radius is km.
+  const radius = map.distance(sw, ne) / 2000;
+  return radius;
+}
+
+function limitValue(val, min=Number.MIN_VALUE, max=Number.MAX_VALUE) {
+  return Math.min(Math.max(min, val), max);
+}
 
 const osm = L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,16 +27,70 @@ const osm = L.tileLayer(
   tileSize: 256,
 });
 
+let center = [34.725401, 137.718005];
+let radius = 18;
 const map = L.map('mapid')
-.addLayer(osm)
-//.fitWorld();
-.setView([34.725401, 137.718005], 18);
+  .addLayer(osm)
+  //.fitWorld();
+  .setView([34.725401, 137.718005], 18);
+L.control.scale().addTo(map);
 
-map.on('click', (e)=>{
-  L.marker(e.latlng)
-  .addTo(map)
-  .bindPopup(`<strong>${e.latlng}</strong>`)
-  .openPopup();
+const database = firebase.database();
+const firebaseRef = database.ref("geoentry");
+const geoFire = new GeoFire(firebaseRef);
+
+const geoQuery = geoFire.query({
+  center: getCenter(map),
+  radius: limitValue(getRadius(map), 0.5, 10),
+});
+
+map.on("moveend", (e) => {
+  geoQuery.updateCriteria({
+    center: getCenter(map),
+    radius: limitValue(getRadius(map), 0.5, 10),
+  });
+});
+
+map.on("zoomend", (e) => {
+  geoQuery.updateCriteria({
+    center: getCenter(map),
+    radius: limitValue(getRadius(map), 0.5, 10),
+  });
+});
+
+const markers = {};
+
+geoQuery.on("key_entered", function(key, location, distance){
+  const marker = L.marker(location)
+    .addTo(map)
+    .bindPopup(`<strong>${key}</strong>`)
+  marker.on('dblclick', e => {
+    //marker.remove();
+    geoFire.remove(key)
+      .catch(error => {
+        console.log(`Couldn't remove ${key} : ${error}`);
+        //marker.addTo(map);
+    });
+  });
+  markers[key] = marker;
+});
+geoQuery.on("key_exited", function(key, location, distance){
+  if (markers.hasOwnProperty(key)) {
+    markers[key].remove();
+    delete markers[key];
+  }
+});
+
+
+map.on('click', e => {
+  const key = `${userid}@${Date.now()}`;
+  geoFire.set(key, [e.latlng.lat, e.latlng.lng])
+    .then(result => {
+      console.log(`set succeeded : ${key}`);
+    })
+    .catch(error => {
+      console.log(`Couldn't set ${key} : ${error}`);
+    });
 });
 
 /*
